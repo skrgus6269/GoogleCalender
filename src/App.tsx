87f9fd97b275aa147832ecf080319a1cf01aff64
skrgus6269 <knh6269@gmail.com>
2024-12-15 17:43:03 +0900

    feat: 헤더 구현

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,41 @@ import Calender from "./components/Calender";
 import Header from "./components/Header";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
+import { useState } from "react";
+import AddScheduleButton from "./components/AddScheduleButton";
+import { scheduleType } from "./type/schedule.stype";
 
 function App() {
+  const [addScheduleModalIsOpen, setAddScheduleModalIsOpen] =
+    useState<boolean>(false);
+
+  const [selectedSchedule, setSelectedSchedule] = useState<scheduleType | null>(
+    null
+  );
+
   return (
     <Provider store={store}>
-      <div className={$.container}>
+      <div
+        className={$.container}
+        onClick={() => {
+          setSelectedSchedule(null);
+          setAddScheduleModalIsOpen(false);
+        }}
+      >
         <Header />
         <div className={$.layout}>
-          <DatePicker />
-          <Calender />
+          <div className={$.left}>
+            <AddScheduleButton
+              setAddScheduleModalIsOpen={setAddScheduleModalIsOpen}
+            />
+            <DatePicker />
+          </div>
+          <Calender
+            selectedSchedule={selectedSchedule}
+            setSelectedSchedule={setSelectedSchedule}
+            addScheduleModalIsOpen={addScheduleModalIsOpen}
+            setAddScheduleModalIsOpen={setAddScheduleModalIsOpen}
+          />
         </div>
       </div>
     </Provider>
